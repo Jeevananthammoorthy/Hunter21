abbbfbdc7d3c0ba67a8afa8834881e01b7a5e663
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2017-06-14 16:44:13 +0530

    Create 21.java

---
+++ 21.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class 21 {
+	public static void main (String[] args) {
+		Scanner in=new Scanner(System.in);
+		String str=in.nextLine();
+		String[] str1=str.split(" ");
+		for(int i=(str1.length-1);i>=0;i--)
+		System.out.print(str1[i]+" ");
+		
+	}
+}
