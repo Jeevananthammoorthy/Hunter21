650de656ab593c6cc3b228dfd0298c50123b53b6
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2017-07-04 21:44:44 -0700

    Update 22.java

--- 22.java
+++ 22.java
@@ -4,7 +4,7 @@ class 22 {
 	public static void main (String[] args) {
 	    Scanner in=new Scanner(System.in);
 	       ArrayList<Integer> al=new ArrayList<Integer>();
-	       for(int i=0;i<5;i++)
+	       for(int i=0;i<50;i++)
 	       {
 	           al.add(in.nextInt());
 	       }
