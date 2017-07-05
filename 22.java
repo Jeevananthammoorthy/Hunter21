49abcd0a62850b98d0be95bc4dabf9185f25ebd9
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2017-07-04 21:44:10 -0700

    Create 22.java

---
+++ 22.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class 22 {
+	public static void main (String[] args) {
+	    Scanner in=new Scanner(System.in);
+	       ArrayList<Integer> al=new ArrayList<Integer>();
+	       for(int i=0;i<5;i++)
+	       {
+	           al.add(in.nextInt());
+	       }
+	       int size=in.nextInt();
+	       Collections.reverse(al);
+	          System.out.println(al);
+	       System.out.println(al.get(3)+"  "+al.get(size-1));
+	       
+	}
+}
