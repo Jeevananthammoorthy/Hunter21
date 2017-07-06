96c95a368fc2ed0d4bf96ad4965d6ae769ef1d82
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2017-07-05 22:05:39 -0700

    Create 15.java

---
+++ 15.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class 15 {
+	public static void main (String[] args){
+	    int max=0,sum=0,start=0,stop=0;
+		Scanner  in=new Scanner(System.in);
+		int size=in.nextInt();
+		int[] arr=new int[size];
+		for(int i=0;i<size;i++)
+		    arr[i]=in.nextInt();
+		    for(int j=0;j<size;j++)
+		     for(int i=j;i<size;i++)
+		          {
+		              sum+=arr[i];
+		              if(sum>max)
+		              {
+		                  start=j;
+		                  stop=i;
+		                  max=sum;
+		              }
+		          }
+		      for(int i=start;i<=stop;i++)
+		      System.out.print(arr[i]+" ");
+	}
+}
